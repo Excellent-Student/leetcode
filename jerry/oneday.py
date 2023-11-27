c152262ae2affbee0854d2637caf1cfe7030f8c3
ningxuankun <ningxuankun@bytedance.com>
2023-11-27 20:22:56 +0800

    change map fix

--- jerry/oneday.py
+++ jerry/oneday.py
@@ -12,20 +12,29 @@
 """
 
 def twoSum(nums, target):
+    """
+    # 此方法只能计算相邻元素的和，不能计算非相邻元素的和
     if len(nums) <=1 :
         raise Exception("num is empty list or nums length is less than 1")
     else:
         for num_index in range(len(nums)):
+            if int(nums[num_index + 1]) > len(nums):
+                return []
             if target - int(nums[num_index]) == int(nums[num_index + 1]):
                 return [num_index, num_index + 1]
-
-
+    """
+    map = {}
+    for index, num in enumerate(nums):
+        complement = target - num
+        if complement in map:
+            return [map[complement], index]
+        map[num] = index
 
 
 if __name__ == '__main__':
     """
     思路 :当列表为空或列表长度小于1时，抛出异常；否则，遍历列表，对于每个元素，计算 target - nums[i]，如果这个值在列表中存在，返回包含这个值的两个元素的索引
     """
-    nums = [2,7,9,12,15]
-    targegt = 9
+    nums = [1, 2, 3, 4, 5, 6, 7]
+    targegt = 13
     print(twoSum(nums=nums,target=targegt))
